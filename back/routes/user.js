624c4d8c2bd2507e67edde73b342e75ed4197636
unknown <junheeyeap@gmail.com>
2022-01-30 01:17:59 +0900

    passport : change routes login into passport

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,52 +1,27 @@
 const router = require("../passport/local");
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const db = require('../config/db');
 const passport = require('passport')
-,LocalStrategy = require('passport-local').Strategy;
 
-const userInfoInsert = async (email, name, password) => {
-	const userInsertQuery = "INSERT INTO USER(email, name, password) values(?, ?, ?)";
-	await db.connection.query(userInsertQuery, [email, name, password]);
-};
-
-router.post("/register", async(req, res) => {
-
-	const email = req.body.email;
-  const name = req.body.name;
-  const encryptedPassowrd = bcrypt.hashSync(req.body.password, 10);
-	try {
-		const result = await userInfoInsert(email, name, encryptedPassowrd);
-	} catch (err) {
-		console.log(err);
-	}
-})
-
-const userInfoFilteredByName = async (name) => {
-	const userFilterQuery = "SELECT * FROM USER WHERE NAME = ?";
-	const result = await db.connection.query(userFilterQuery, [name]);
-	return result[0];
-};
-
-passport.use(
-	new LocalStrategy(
-			async (name, password, done) => {
-					const result = await userInfoFilteredByName(name);
-				
-					if (result.length > 0) {
-							const user = result[0];
-							if (user.password === password) {
-									return done(null, user);
-							} else {
-									return done(null, false, {message: "틀린 비밀번호입니다"});
-							}
-					} else {
-							return done(null, false, {message: "존재하지 않는 유저입니다"});
-					}
+router.post('/login', (req ,res) => {
+	//local 전략에서의 done의 세 parameter가 콜백으로 들어온다
+	passport.authenticate('local', (err, user, info) => {
+		if (err) {
+			console.log(err);
+			return 
+		}
+		if (info) { //client error
+			console.log(info);
+			return res.status(401).send(info);
+		}
+		return req.login(user, async (loginErr) => {
+			if (loginErr) {
+				console.error(loginErr);
 			}
-	)
-);
-
+			return res.json(user);
+		});
+	})(req, res)
+})
 
 module.exports = router;
